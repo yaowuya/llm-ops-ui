45e7d727c37cc34de9df8fe1d9263ac4b0a305b0
yaowuya <zrfyaowuya@126.com>
2025-04-08 23:59:46 +0800

    feature:应用编排页面结构与样式设计

--- src/views/space/apps/DetailView.vue
+++ src/views/space/apps/DetailView.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
-import { post } from '@/utils/request.ts'
-
-async function debug() {
-  const response = await post('/app/b31bb8e6-0821-47cd-aa58-3c8e2711d44c/debug', {
-    body: { query: '请问你是谁？' }
-  })
-  console.log(response)
-}
 </script>
 
 <template>
-  <p>这是详情页</p>
-  <a-button type="primary" @click="debug">发起请求</a-button>
+  <!-- 最外层容器，高度撑满整个浏览器屏幕 -->
+  <div class="min-h-screen">
+    <!--    顶部导航-->
+    <header class="flex items-center h-[74px] bg-gray-100 border-b border-gray-200 px-4">
+      顶部导航
+    </header>
+    <!--    底部内容区-->
+    <div class="flex flex-row h-[calc(100vh-74px)]">
+      <!--      左侧的编排-->
+      <div class="w-2/3 bg-gray-50 h-full">
+        左侧的编排
+      </div>
+      <!--      右侧调试与预览-->
+      <div class="flex flex-col w-1/3 bg-white h-full">
+        右侧调试与预览
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
